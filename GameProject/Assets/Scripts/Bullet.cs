a533bab622d0fff23ff68ecdc5538302d3908da5
Hendrik Bell <henne_bell@gmx.de>
2019-01-26 00:01:18 +0100

    Shooting and Bullets

---
+++ GameProject/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{   
+
+    public float speed;
+    public Vector3 dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+         Destroy(gameObject, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.position += speed * dir; 
+    }
+
+        void OnTriggerEnter (Collider col)
+    {   
+        Destroy(gameObject);
+        print ("Player collided with" + col.GetComponent<Collider>().gameObject.name);
+    }
+}
