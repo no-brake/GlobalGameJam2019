f00c829e8c4c0d42a488bc01c207a1c3f42ce629
Kristof <kabumm27@live.de>
2019-01-26 00:13:18 +0100

    Basic Main Menu

---
+++ GameProject/Assets/Scripts/MainMenuHandling.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandling : MonoBehaviour
+{
+
+    public Button btnStartGame;
+    public Button btnExitGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnStartGame.onClick.AddListener(StartGame);
+        btnExitGame.onClick.AddListener(ExitGame);
+    }
+
+    void StartGame()
+    {
+        Debug.Log("Start game");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void ExitGame()
+    {
+        Debug.Log("Exit game");
+    }
+}
