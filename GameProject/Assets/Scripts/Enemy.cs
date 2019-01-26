fa21188e0e5d27436b5809a7002202c0a7fe3f5c
Hendrik Bell <henne_bell@gmx.de>
2019-01-26 11:14:35 +0100

    Damage, automic shooting

--- GameProject/Assets/Scripts/Enemy.cs
+++ GameProject/Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
 {   
     public Vector3 target;
     public float speed;
+    public int health;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,10 @@ public class Enemy : MonoBehaviour
     {
         Vector3 dir = Vector3.Normalize(target - transform.position); 
         transform.position += speed * dir;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
--- GameProject/Assets/Scripts/Shooting.cs
+++ GameProject/Assets/Scripts/Shooting.cs
@@ -9,36 +9,28 @@ public class Shooting : MonoBehaviour
     int bulletNum;
     public Bullet Bullet;
     bool canShoot, isShooting;
+    int frequence;
     // Start is called before the first frame update
     void Start()
     {
-        
+        frequence = 25;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if((Input.GetMouseButtonDown(0) || isShooting)  && canShoot) 
+       if(Input.GetMouseButtonDown(0)  && canShoot) 
        {
-           Vector3 mousePos = Input.mousePosition ;
-           mousePos.z = 20;
-           
-           mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        
-           Transform trans = this.gameObject.transform;
-
-           Vector3 pos = mousePos - trans.position;
-           pos.y = 0;
-           pos = Vector3.Normalize(pos);
-           Bullet bullet = Instantiate(Bullet,trans.position + pos,trans.rotation);
-           bullet.speed = 0.8F;
-           bullet.dir = pos;
-           isShooting = true;
-          
+            createBullet();
+            isShooting = true;
        }   
        if(Input.GetMouseButtonUp(0))
        {
            isShooting = false;
+       }
+       if(isShooting && canShoot &&  Time.frameCount % frequence == 0)
+       {
+           createBullet();
        }
     }
 
@@ -59,5 +51,23 @@ public class Shooting : MonoBehaviour
             print("you can't shoot");
             canShoot = false;
         }
+    }
+
+    void  createBullet()
+    {
+        Vector3 mousePos = Input.mousePosition ;
+           
+        mousePos.z = 20;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        
+        Transform trans = this.gameObject.transform;
+
+        Vector3 pos = mousePos - trans.position;
+        pos.y = 0;
+        pos = Vector3.Normalize(pos);
+        Bullet bullet = Instantiate(Bullet,trans.position + pos,trans.rotation);
+        bullet.speed = 0.6F;
+        bullet.dir = pos;
+        bullet.damage = 37;
     }
 }
