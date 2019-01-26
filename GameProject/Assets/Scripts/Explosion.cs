6b42f90804a3ac2153a423d46dc97351344584a8
Hendrik Bell <henne_bell@gmx.de>
2019-01-26 23:12:44 +0100

    Rocket falls wir Zeit und Bock haben

---
+++ GameProject/Assets/Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public ParticleSystem part;
+    // Start is called before the first frame update
+    public int damage;
+
+    void Start()
+    {
+       Explode(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += new Vector3(1,1,1);
+    }
+    void Explode() {
+        var exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject, exp.duration);
+    }
+}
