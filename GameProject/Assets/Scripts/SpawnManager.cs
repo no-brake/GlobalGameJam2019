6a44ecc9ca502a3f5a982074b250594e327211b7
Kristof <kabumm27@live.de>
2019-01-26 21:28:01 +0100

    Watcher Audio Stuff + Spawn Manager

--- GameProject/Assets/Scripts/SpawnManager.cs
+++ GameProject/Assets/Scripts/SpawnManager.cs
@@ -4,36 +4,56 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public Enemy Enemy;
-    public List<Spawner> spawner = new List<Spawner>();
+    public Enemy Watcher;
+    public Enemy DeceasedPhantom;
+    public Enemy Oriax;
+    public List<Spawner> spawners = new List<Spawner>();
 
     float timeToSpawn;
     float hardcodedRandomGeneratedFactorForSimultanousSpawnPrevention;
+
+    private GameState gameState;
 
     // Start is called before the first frame update
     void Start()
     {
         hardcodedRandomGeneratedFactorForSimultanousSpawnPrevention = Random.Range(0f, 3f);
         timeToSpawn = 5F + hardcodedRandomGeneratedFactorForSimultanousSpawnPrevention;
+
+        GameObject gameStateObject = GameObject.Find("GameState");
+        this.gameState = gameStateObject.GetComponent<GameState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Spawner selectedSpawner = spawner[0];
+        int spawnerId = (int)Random.Range(0f, (float)spawners.Count);
+
+        Spawner selectedSpawner = spawners[spawnerId];
+        GameObject target = selectedSpawner.target;
+
         timeToSpawn -= Time.deltaTime;
         if (timeToSpawn <= 0)
         {
-            float scale = Mathf.Max(transform.localScale.x, transform.localScale.z);
-            float ran = Random.Range(-1.0F, 1.0F);
-            Vector3 pos = transform.position + ran * new Vector3(0, 0, scale * 5);
-            Vector3 dir = -(pos + new Vector3(0, 1, 0)) + selectedSpawner.target.transform.position;
-            Enemy enemy = Instantiate(Enemy, pos + new Vector3(0, 1, 0), Quaternion.LookRotation(dir, new Vector3(0, 1, 0)));
-            enemy.target = selectedSpawner.target.transform.position;
-            enemy.speed = 0.1F;
-            enemy.health = 100;
+            gameState.spawnCount++;
 
-            timeToSpawn = 5F + hardcodedRandomGeneratedFactorForSimultanousSpawnPrevention;
+            if (gameState.spawnCount >= 50)
+            {
+                selectedSpawner.SpawnEnemy(Oriax, 0.04f, 1000);
+            } else
+            {
+                float randomSpawnChance = Random.Range(0f, 1f);
+                if (randomSpawnChance < 0.80)
+                {
+                    selectedSpawner.SpawnEnemy(Watcher, 0.1f, 100);
+                }
+                else
+                {
+                    selectedSpawner.SpawnEnemy(DeceasedPhantom, 0.08f, 500);
+                }
+            }
+
+            timeToSpawn = 3f;
         }
     }
 }
--- GameProject/Assets/Scripts/Spawner.cs
+++ GameProject/Assets/Scripts/Spawner.cs
@@ -4,58 +4,18 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public Enemy Watcher;
-    public Enemy DeceasedPhantom;
-    public Enemy Oriax;
     public GameObject target;
-    float timeToSpawn;
-    private GameState gameState; 
      
-    // Start is called before the first frame update
-    void Start()
+    public void SpawnEnemy(Enemy enemy, float speed, int health)
     {
-        timeToSpawn = 5F;
-        GameObject gameStateObject = GameObject.Find("GameState");
-        this.gameState = gameStateObject.GetComponent<GameState>();
-    }
+        float ran = Random.Range(-1.0F, 1.0F);
+        float scale = Mathf.Max(transform.localScale.x, transform.localScale.z);
+        Vector3 pos = transform.position + ran * new Vector3(0, 0, scale * 5);
+        Vector3 dir = -(pos + new Vector3(0, 1, 0)) + target.transform.position;
 
-    // Update is called once per frame
-    void Update()
-    {
-
-        if(gameState.spawnCount == 50){
-            Enemy ori = Instantiate(Oriax,transform.position + new Vector3(0,1,0),
-                         Quaternion.LookRotation(-(transform.position + new Vector3(0,1,0)) + target.transform.position, new Vector3(0,1,0))); 
-            ori.target = target.transform.position;
-            ori.speed = 0.04F;
-            ori.health = 1000;
-            gameState.spawnCount++;
-            return;
-        }
-        timeToSpawn -= Time.deltaTime;
-        if(timeToSpawn <= 0 && gameState.spawnCount < 50)
-        {  
-            gameState.spawnCount++;  
-            float scale = Mathf.Max(transform.localScale.x,transform.localScale.z);
-            float ran   = Random.Range(-1.0F,1.0F);
-            
-            Vector3 pos = transform.position + ran * new Vector3(0,0,scale*5);
-            Vector3 dir = -(pos + new Vector3(0,1,0)) + target.transform.position;
-            if(ran < 0.75){
-                Enemy watcher = Instantiate(Watcher,pos + new Vector3(0,1,0), Quaternion.LookRotation(dir, new Vector3(0,1,0))); 
-                watcher.target = target.transform.position;
-                watcher.speed = 0.1F;
-                watcher.health = 100;
-                timeToSpawn = 5F;
-            }else{
-                Enemy dPhan = Instantiate(DeceasedPhantom,pos + new Vector3(0,1,0), Quaternion.LookRotation(dir, new Vector3(0,1,0))); 
-                dPhan.target = target.transform.position;
-                dPhan.speed = 0.08F;
-                dPhan.health = 500;
-                timeToSpawn = 5F; 
-            }
-
-            
-        }
+        Enemy spawnedEnemy = Instantiate(enemy, pos + new Vector3(0, 1, 0), Quaternion.LookRotation(dir, new Vector3(0, 1, 0)));
+        spawnedEnemy.target = target.transform.position;
+        spawnedEnemy.speed = speed;
+        spawnedEnemy.health = health;
     }
 }
