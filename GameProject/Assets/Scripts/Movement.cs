bcd0d6c9f7641741aa145753f021f86b72822ed7
no-brake <k.mannstedt@gmail.com>
2019-01-26 02:22:26 +0100

    use either mouse or gamepad (check in Start() method)

--- GameProject/Assets/Scripts/Movement.cs
+++ GameProject/Assets/Scripts/Movement.cs
@@ -8,10 +8,17 @@ public class Movement : MonoBehaviour
     public int rotationSpeed = 10;
     public int rotationOffset = 180;
 
+    bool controllerConnected = false;
+
     // Start is called before the first frame update
     void Start()
-    {
-
+    {   
+        for (int i = 0; i < Input.GetJoystickNames().Length; i++){
+            if (Input.GetJoystickNames()[i].Length > 0) {
+                controllerConnected = true;
+                break;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -23,23 +30,24 @@ public class Movement : MonoBehaviour
 
         transform.Translate(new Vector3(xLeft * Time.deltaTime * transformSpeed , 0, -yLeft * Time.deltaTime * transformSpeed), Space.World);
 
-        //-----Rotate the player -> with Mouse
+        if(!controllerConnected) {
+            //-----Rotate the player -> with Mouse
+            //Get the Screen positions of the object
+            Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+            //Get the Screen position of the mouse
+            Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            //Get the angle between the points
+            float a = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+            transform.rotation =  Quaternion.Euler (new Vector3(0f, -a + rotationOffset, 0f));
+        } else {
+            //-----Rotate the player -> with gamepad
+            float xRight = Input.GetAxis("HorizontalRight");
+            float yRight = Input.GetAxis("VerticalRight");
 
-        //Get the Screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        //Get the angle between the points
-        float a = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-        transform.rotation =  Quaternion.Euler (new Vector3(0f, -a + rotationOffset, 0f));
-
-        //-----Rotate the player -> with gamepad
-        float xRight = Input.GetAxis("HorizontalRight");
-        float yRight = Input.GetAxis("VerticalRight");
-
-        if (xRight != 0.0f || yRight != 0.0f) {
-            float angle = Mathf.Atan2(yRight, xRight) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0f, angle, 0f);
+            if (xRight != 0.0f || yRight != 0.0f) {
+                float angle = Mathf.Atan2(yRight, xRight) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0f, angle, 0f);
+            }
         }
 
     }
