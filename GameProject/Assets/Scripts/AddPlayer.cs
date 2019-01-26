ec83e0e26c0a79b57d7933c3d4262bc1070def4d
no-brake <k.mannstedt@gmail.com>
2019-01-26 17:04:16 +0100

    depending on connected controllers, add players (max 2)

---
+++ GameProject/Assets/Scripts/AddPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayer : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+    public Vector3 startPlayer1;
+    public Vector3 startPlayer2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(player1);
+        print(player2);
+
+        bool player1HasController = false;
+        bool player2HasController = false;
+
+        for(int i = 0; i < Mathf.Min(Input.GetJoystickNames().Length, 2); i++){
+            bool v = false;
+            if(Input.GetJoystickNames()[i].Length > 0){
+                v = true;
+            }
+            switch(i){
+                case 0:
+                    player1HasController = v;
+                    break;
+                case 1:
+                    player2HasController = v;
+                    break;
+            }
+        }
+
+        print(player1HasController + ", " + player2HasController);
+
+        if(player1HasController && player2HasController) {
+            GameObject p1 = Instantiate(player1, startPlayer1, Quaternion.identity);
+            p1.GetComponent<Movement>().controller = 0;
+            GameObject p2 = Instantiate(player2, startPlayer2, Quaternion.identity);
+            p2.GetComponent<Movement>().controller = 1;
+            // both use controller
+        } else if(player1HasController || player2HasController) {
+            // assign player1 a controller and player2 use keyboard and mouse
+            GameObject p1 = Instantiate(player1, startPlayer1, Quaternion.identity);
+            p1.GetComponent<Movement>().controller = 0;
+            GameObject p2 = Instantiate(player2, startPlayer2, Quaternion.identity);
+            p2.GetComponent<Movement>().controller = 1;
+        } else {
+            // only one player plays the game
+            GameObject p1 = Instantiate(player1, startPlayer1, Quaternion.identity);
+            p1.GetComponent<Movement>().controller = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
