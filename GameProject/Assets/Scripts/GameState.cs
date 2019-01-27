7036e3a7f7734d0672e4958e4ca9415324a1439f
Julian Gill <julian.gill@web.de>
2019-01-27 13:06:23 +0100

    Hello Darkness my old freind.

--- GameProject/Assets/Scripts/GameState.cs
+++ GameProject/Assets/Scripts/GameState.cs
@@ -12,22 +12,53 @@ public class GameState : MonoBehaviour
     public float houseHealth;
     public int spawnCount;
 
+    private Color alpha;
+    public float timeToFade = 1.0f;
+    public GameObject fadePlane;
+
+    private Text dayText;
+
+    private Color dayTextColor;
+
     // Start is called before the first frame update
     void Start()
     {
         this.kills = 0;
         this.houseHealth = 10;
         this.spawnCount = 0;
+
+        //if (GlobalState.day <= 3) {
+        alpha = fadePlane.GetComponent<MeshRenderer>().material.color;
+        alpha.a = 0;
+        dayText = fadePlane.GetComponentInChildren<Text>();
+        dayText.text = "Day " + GlobalState.day;
+        dayTextColor = dayText.color;
+        dayTextColor.a = 0;
+        //}
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        fadePlane.GetComponent<MeshRenderer>().material.color = Color.Lerp(fadePlane.GetComponent<MeshRenderer>().material.color, alpha, timeToFade*Time.deltaTime);
+        dayText.color = Color.Lerp(dayText.color, dayTextColor, timeToFade * Time.deltaTime * 2);
+
         if(this.houseHealth <= 0)
-        {   
-            GlobalState.day++;
-            SceneManager.LoadScene("SampleScene");
+        {
+            dayText.text = "Day " + (GlobalState.day + 1);
+            dayTextColor.a = 1;
+            alpha.a = 1;
+            fadePlane.GetComponent<MeshRenderer>().material.color = Color.Lerp(fadePlane.GetComponent<MeshRenderer>().material.color, alpha, timeToFade*Time.deltaTime);
+            dayText.color = Color.Lerp(dayText.color,dayTextColor, timeToFade * Time.deltaTime * 2);
+            Debug.Log("DayTextColor: " + dayText.color.a); 
+            if(dayText.color.a > 0.9) {
+                GlobalState.day++;
+                SceneManager.LoadScene("SampleScene");
+            }
             //text.text = "DU BIST TOT!";
         }
+
     }
 }
